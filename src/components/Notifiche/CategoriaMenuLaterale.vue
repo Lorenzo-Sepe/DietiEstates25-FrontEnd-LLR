7062da1ed94bf63dbaa4c73e06e9cccfcb98d796
Sperob98 <rbrna98@gmail.com>
2025-03-27 01:45:58 +0100

    Aggiunta logica nei bottoni disattiva/attiva del menu laterale

--- src/components/Notifiche/CategoriaMenuLaterale.vue
+++ src/components/Notifiche/CategoriaMenuLaterale.vue
@@ -1,20 +1,69 @@
 <template>
 
-    <div class="flex justify-between items-center p-1 gap-4 w-full hover:bg-[#ECFDF5]/60! cursor-pointer">
-        <div class="flex-1 flex flex-col">
-            <span class="font-medium">{{ props.categoria.nomeCategoria }}</span>
-            <span class="text-sm text-surface-500">{{ props.categoria.descrizioneCategoria }}</span>
+        <Dialog v-model:visible="dialogConferma" modal header="ATTENZIONE" :style="{ width: 'auto' }"
+            :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+            <p>Non riceverai ulteriori notifiche relative a questa categoria</p>
+            <div class="flex justify-end gap-2 my-4">
+                <Button label="Annulla" severity="secondary" raised @click="dialogConferma = false" />
+                <Button label="Conferma" severity="success" raised @click="clickConferma" />
+            </div>
+        </Dialog>
+
+        <div class="flex justify-between items-center p-1 gap-4 w-full hover:bg-[#ECFDF5]/60! cursor-pointer">
+            <div class="flex-1 flex flex-col"
+                @click="clickCategoria(props.categoria.nomeCategoria, props.categoria.idCategoria)">
+                <span class="font-medium">{{ props.categoria.nomeCategoria }}</span>
+                <span class="text-sm text-surface-500">{{ props.categoria.descrizioneCategoria }}</span>
+            </div>
+            <Button class="w-18 h-10 text-xs sm:text-sm" v-if="props.categoria.attivo" label="Disattiva"
+                @click="clickDisattiva(props.categoria)" />
+            <Button class="w-18 h-10 text-xs sm:text-sm" v-else label="Attiva" @click="clickAttiva(props.categoria)" />
         </div>
-        <Button class="w-18 h-10 text-xs sm:text-sm"  v-if="props.categoria.attivo" label="Disattiva" /> 
-        <Button class="w-18 h-10 text-xs sm:text-sm" v-else label="Attiva" />
-    </div>
 
 </template>
 
 <script setup>
-import { ref, defineProps } from 'vue';
+import { ref, defineProps, defineEmits } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
 import { Button } from 'primevue';
+import Dialog from 'primevue/dialog';
+
 
 const props = defineProps(['categoria']);
+const emit = defineEmits(['modificaSottoscrizione']);
+
+const router = useRouter();
+const route = useRoute();
+
+const dialogConferma = ref(false);
+const idCategoriaSelected = ref(null);
+
+const clickCategoria = (nomeCategoria, idCategoria) => {
+
+    router.push({
+        path: route.path,
+        query: { ...route.query, nome: nomeCategoria, id: idCategoria, pagina: 0 }
+    });
+}
+
+const clickDisattiva = (categoria) => {
+
+    idCategoriaSelected.value = categoria.idCategoria;
+    dialogConferma.value = true;
+}
+
+const clickAttiva = (categoria) => {
+
+    idCategoriaSelected.value = categoria.idCategoria;
+    emit('modificaSottoscrizione', idCategoriaSelected.value);
+}
+
+const clickConferma = () => {
+
+    dialogConferma.value = false;
+
+    emit('modificaSottoscrizione', idCategoriaSelected.value);
+}
 
 </script>
--- src/components/Notifiche/MenuLaterale.vue
+++ src/components/Notifiche/MenuLaterale.vue
@@ -1,9 +1,22 @@
 <template>
 
+    <Dialog v-model:visible="impostazioniSottoscrizineVisibe" modal :style="{ width: 'auto' }"
+        :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+        <template #container="{ closeCallback }">
+            <div class="flex flex-col p-4">
+                <h2 class="text-lg font-bold">DISATTIVA/ATTIVA NOTIFICHE</h2>
+                <ImpostazioniSottoscrizioni :categorie="props.categorie"
+                    :dialogSottoscrizioni="impostazioniSottoscrizineVisibe" @chiudiImpostazioni="closeCallback"
+                    @modificaSottoscrizioni="emit('modificaSottoscrizioni');
+                    " />
+            </div>
+        </template>
+    </Dialog>
+
     <div
         class="riga-tutteLeNotifiche flex justify-between items-center p-1 gap-4 w-full hover:bg-[#ECFDF5]/60! cursor-pointer">
-        <span>Tutte le tue notifiche</span>
-        <Button variant="text" rounded aria-label="Filter"
+        <span @click="clickTutteLeCategorie">Tutte le tue notifiche</span>
+        <Button variant="text" rounded aria-label="Filter" @click="impostazioniSottoscrizineVisibe = true"
             v-tooltip="{ value: 'Impostazioni sottoscrizioni', showDelay: 300, hideDelay: 300 }">
             <template #icon>
                 <img src="../../assets/Icon/menuSottoscrizione.png" class="w-5 h-5" />
@@ -16,13 +29,13 @@
                 <AccordionHeader>{{ getTitle() }}</AccordionHeader>
                 <AccordionContent class="text-left items-start">
                     <CategoriaMenuLaterale v-for="categoria in filteredCategorieAttive" :categoria="categoria"
-                        @click="clickCategoria(categoria.nomeCategoria, categoria.idCategoria)" />
+                        @modificaSottoscrizione="modificaSottoscrizione" />
 
                     <div class="flex flex-col items-start my-4">
                         <label class="font-bold">Categorie disattivate</label>
                         <div class="border-t-2 border-black w-full mx-auto my-2"></div>
                         <CategoriaMenuLaterale v-for="categoria in filteredCategorieDisattivate" :categoria="categoria"
-                            @click="clickCategoria(categoria.nomeCategoria, categoria.idCategoria)" />
+                            @modificaSottoscrizione="modificaSottoscrizione" />
                     </div>
 
                 </AccordionContent>
@@ -33,7 +46,7 @@
 </template>
 
 <script setup>
-import { ref, defineProps, computed } from 'vue';
+import { ref, defineProps, defineEmits, computed, onMounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 
 import Accordion from 'primevue/accordion';
@@ -41,16 +54,26 @@ import AccordionPanel from 'primevue/accordionpanel';
 import AccordionHeader from 'primevue/accordionheader';
 import AccordionContent from 'primevue/accordioncontent';
 import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
 import 'primeicons/primeicons.css'
 
 
 import CategoriaMenuLaterale from '../Notifiche/CategoriaMenuLaterale.vue';
+import ImpostazioniSottoscrizioni from '../Notifiche/ImpostazioniSottoscrizioni.vue';
 
 const accordionChiuso = ref(null);
 const props = defineProps(['categorie']);
+const emit = defineEmits(['modificaSottoscrizioni']);
 
 const router = useRouter();
 const route = useRoute();
+
+const impostazioniSottoscrizineVisibe = ref(false);
+
+onMounted(() => {
+
+
+});
 
 
 const getTitle = () => {
@@ -76,12 +99,27 @@ const filteredCategorieDisattivate = computed(() => {
 
 });
 
-const clickCategoria = (nomeCategoria, idCategoria) => {
+const clickTutteLeCategorie = () => {
 
     router.push({
         path: route.path,
-        query: { ...route.query, nome: nomeCategoria, id: idCategoria, pagina: 0 }
+        query: { ...route.query, nome: "Tutte le categorie", id: 0, pagina: 0 }
     });
+}
+
+const modificaSottoscrizione = (idCategoria) => {
+
+    console.log("sono nel log");
+
+    props.categorie.forEach(categoria => {
+
+        if (categoria.idCategoria === idCategoria) {
+
+            categoria.attivo = !categoria.attivo;
+        }
+    });
+
+    emit('modificaSottoscrizioni');
 }
 
 </script>
