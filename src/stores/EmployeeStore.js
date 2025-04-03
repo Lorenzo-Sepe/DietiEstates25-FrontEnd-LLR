58f611609b1ae25caa16806d31942ec5c69738f3
Sperob98 <rbrna98@gmail.com>
2025-04-03 23:25:20 +0200

    Aggiunto nella get dello store dell'employee i dati della agenzia

--- src/stores/EmployeeStore.js
+++ src/stores/EmployeeStore.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 import { UserInfoResponse } from '../dto/Response/UserInfoResponse';
 import { DatiImpiegatoResponse } from '../dto/Response/DatiImpiegato';
 import { DatiAgenziaImmobiliareResponse } from '../dto/Response/DatiAgenziaImmobiliareResponse';
-import { getDefaultAvatar , getDatiUser,getDatiImpiegato} from '../services/UserService';
+import { getDefaultAvatar, getDatiUser, getDatiImpiegato } from '../services/UserService';
 import { getDatiAgenziaByEmailDipendente } from '../services/AgenziaImmobiliareService';
 
 export const useEmployeeStore = defineStore('employee', {
@@ -12,7 +12,7 @@ export const useEmployeeStore = defineStore('employee', {
       token: "",
       Info: new UserInfoResponse(),
       datiImpiegato: new DatiImpiegatoResponse(),
-      DatiAgenziaImmobiliare : new DatiAgenziaImmobiliareResponse()
+      DatiAgenziaImmobiliare: new DatiAgenziaImmobiliareResponse()
 
     }
   }),
@@ -22,8 +22,12 @@ export const useEmployeeStore = defineStore('employee', {
     UrlFotoProfilo: (state) => {
       // Controlla se l'URL della foto del profilo è disponibile
       return state.employee.Info.UrlFotoProfilo || getDefaultAvatar(state.employee.email);
-    }
     },
+    dipedenti: (state) => {
+
+      return state.employee.DatiAgenziaImmobiliare.dipendenti || []; 
+    }
+  },
   actions: {
     impostaImpiegato(email, token) {
       this.employee.email = email;
@@ -49,23 +53,23 @@ export const useEmployeeStore = defineStore('employee', {
           console.warn("Attenzione: si è verificato un errore durante l'aggiornamento delle info.", error);
           this.employee.Info.UrlFotoProfilo = getDefaultAvatar(this.employee.email);
         });
-        getDatiImpiegato(email)
+      getDatiImpiegato(email)
         .then((dati) => {
-          console.log("get dati impiegato: ",dati);
+          console.log("get dati impiegato: ", dati);
           this.employee.datiImpiegato = dati;
         })
         .catch((error) => {
           console.warn("Attenzione: si è verificato un errore durante l'aggiornamento dei dati impiegato", error);
         });
-        getDatiAgenziaByEmailDipendente(email)
+      getDatiAgenziaByEmailDipendente(email)
         .then((dati) => {
           this.employee.DatiAgenziaImmobiliare = dati;
         })
         .catch((error) => {
           console.warn("Attenzione: si è verificato un errore durante l'aggiornamento dei dati riferti alla agenzia", error);
         });
-      
-      
+
+
     }
   },
   persist: true // Abilita la persistenza dello stato
