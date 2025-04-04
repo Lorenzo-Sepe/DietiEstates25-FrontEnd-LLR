4292418afbf147f6d459dc2b01ce1f0d32c426c5
Sperob98 <rbrna98@gmail.com>
2025-04-04 18:25:12 +0200

    Spostato test-roberto in pannello-staff

--- src/views/pannelloStaff.vue
+++ src/views/pannelloStaff.vue
@@ -1,6 +1,7 @@
 <template>
 
-<!----------------------------------------------  SEZIONE DIALOG PER GLI ALLERT ------------------------------------------------>
+
+    <!----------------------------------------------  SEZIONE DIALOG PER GLI ALLERT ------------------------------------------------>
     <Dialog v-model:visible="loadingOperazione" header="OPERAZIONE IN CORSO" :style="{ width: 'auto' }"
         :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
         <div class="card flex justify-center">
@@ -26,32 +27,52 @@
 
     <!------------------------------------------------------------------------ ------------------------------------------------>
 
-    <div class="flex flex-col gap-4 w-full p-2">
-        <AreaSuperiore />
-        <div class="w-full">
-            <TabellaAnnunci :propAnnunci="annunci" :propLoading="loading" :propostaRequest="propostaRequest"
-                @nuovaProposta="aggiungiPropostaManuale" 
-                @eliminaProposta="rifiutaProposta"
-                @accettaProposta="accettaProposta"
-                @controproposta="controproposta" />
+    <div class="w-full flex flex-col gap-2">
+
+        <AreaSuperiore class="w-full items-center justify-center" />
+
+        <ScheletroListaAgenti v-if="loadingListaAgenti" />
+
+        <div v-else class="w-full p-4">
+            <h2 v-if="!isAgente">Lista agenti dell'agenzia:</h2>
+            <Accordion v-model:activeIndex="activeIndex" :multiple="false" @update:activeIndex="onAccordionToggle"
+                expandIcon="pi pi-plus" collapseIcon="pi pi-minus">
+                <AccordionPanel class="my-2" v-for="(agente, index) in agenti" :value="index">
+                    <AccordionHeader class="!bg-gray-100 hover:bg-green-400!">
+                        <span class="flex items-center gap-2 w-full">
+                            <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+                                shape="circle" />
+                            <span class="font-bold whitespace-nowrap">{{ agente.infoUtente.nomeVisualizzato }}</span>
+                        </span>
+                    </AccordionHeader>
+                    <AccordionContent>
+                        <TabellaAnnunci :propAnnunci="annunci" :propLoading="loading" :propostaRequest="propostaRequest" :isAgente="isAgente"
+                            :agente="employeeStore.dipedenti" @nuovaProposta="aggiungiPropostaManuale"
+                            @eliminaProposta="rifiutaProposta" @accettaProposta="accettaProposta"
+                            @controproposta="controproposta" />
+                    </AccordionContent>
+                </AccordionPanel>
+            </Accordion>
         </div>
-        <div class="card">
-            <Paginator :rows="5" :totalRecords="numeroAnnunci" @page="onPage"></Paginator>
-        </div>
+
     </div>
 
 </template>
 
 <script setup>
-
 import { ref, reactive, onMounted, } from 'vue';
 
-import AreaSuperiore from '../components/PannelloStaff/AreaSuperiore.vue'
-import TabellaAnnunci from '../components/PannelloStaff/tabellaAnnunci.vue'
-import Paginator from 'primevue/paginator';
+import Accordion from 'primevue/accordion';
+import AccordionPanel from 'primevue/accordionpanel';
+import AccordionHeader from 'primevue/accordionheader';
+import AccordionContent from 'primevue/accordioncontent';
+import Avatar from 'primevue/avatar';
 import Dialog from 'primevue/dialog';
 import ProgressSpinner from 'primevue/progressspinner';
 
+import AreaSuperiore from '../components/PannelloStaff/AreaSuperiore.vue'
+import ScheletroListaAgenti from '../components/PannelloStaff/ScheletroListaAgenti.vue'
+import TabellaAnnunci from '../components/PannelloStaff/tabellaAnnunci.vue'
 
 import AnnunciService from '../services/TabellaAnnunciService';
 import PropostaService from '../services/PropostaService';
@@ -60,34 +81,76 @@ import { PropostaRequest } from '../dto/PropostaRequest';
 import { useEmployeeStore } from '../stores/EmployeeStore';
 import { Button } from 'primevue';
 
+const activeIndex = ref(null);
 const numeroAnnunci = ref(0);
 const annunci = ref([]);
 const loading = ref(true);
+const loadingListaAgenti = ref(true);
 const employeeStore = useEmployeeStore();
 const okAllert = ref(false);
 const erroreAllert = ref(false);
 const loadingOperazione = ref(false);
+const agenti = ref([]);
+const isAgente = ref(false);
 
 const filtroAnnunci = reactive(new FiltroAnnuncioRequest());
 const propostaRequest = reactive(new PropostaRequest());
 
 onMounted(async () => {
 
-    filtroAnnunci.numeroPagina = 1;
-    filtroAnnunci.numeroDiElementiPerPagina = 5;
+    try {
 
-    console.log("filtroooo:", filtroAnnunci);
+        await employeeStore.aggiorna();
 
-    getNumeroAnnunci();
-    getAnnunci();
+    } catch (error) {
 
-})
+        console.log("errore durante l'aggiormamento dati pinia: ", error);
+        return;
 
-const onPage = (event) => {
+    } finally {
 
-    filtroAnnunci.numeroPagina = event.page + 1;
-    getAnnunci();
+        if (employeeStore.ruolo === 'MANAGER') {
+
+            agenti.value = filterAgenti(Array.from(employeeStore.employee.DatiAgenziaImmobiliare.dipendentiDettagli.values()));
+            loadingListaAgenti.value = false;
+
+        } else {
+
+            const dettagliAgente = ref(
+
+                {
+                    infoUtente: {
+
+                        nomeVisualizzato: employeeStore.infoUtente.nomeVisualizzato,
+                        email: employeeStore.infoUtente.email,
+                        telefono: employeeStore.infoUtente.telefono,
+                        tipoAccount: employeeStore.infoUtente.tipoAccount
+                    }
+                }
+            );
+
+            agenti.value.push(dettagliAgente.value);
+            isAgente.value = true;
+            loadingListaAgenti.value = false;
+
+        }
+
+    }
+
+});
+
+const filterAgenti = (dipedenti) => {
+
+    return dipedenti ? dipedenti.filter(dipedente => dipedente.infoUtente.tipoAccount === 'AGENT') : [];
 };
+
+const onAccordionToggle = (newIndex) => {
+    if (newIndex !== null) {
+        console.log("Dati utente:", agenti.value[newIndex].infoUtente.email);
+        filtroAnnunci.agenteCreatoreAnnuncio = agenti.value[newIndex].infoUtente.email;
+        getAnnunci();
+    }
+}
 
 const getNumeroAnnunci = async () => {
 
@@ -106,6 +169,7 @@ const getAnnunci = async () => {
 
     try {
 
+        annunci.value = [{}];
         loading.value = true;
         annunci.value = await AnnunciService.getAnnunciByStaff(filtroAnnunci);
 
