5751005682f40cc4cdfe6a0b8e6d165d8410a5ae
Sperob98 <rbrna98@gmail.com>
2025-04-17 00:06:46 +0200

    fix scheletro

---
+++ src/components/DettaglioAnnuncio/ScheletroPagina.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full h-full flex flex-col lg:flex-row gap-2">
+
+        <Skeleton class="ml-4" width="100%" height="30rem"></Skeleton>
+
+        <Skeleton width="100%" height="15rem"></Skeleton>
+
+    </div>
+</template>
+
+<script setup>
+import Skeleton from 'primevue/skeleton';
+
+</script>
