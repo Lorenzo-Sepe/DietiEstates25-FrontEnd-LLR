82339ccd446235e42c5fec661f5657e43c4b2879
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-05-07 18:10:55 +0200

    iniziata modifica per togliere i bottoni di login e registrazione quando si è effettuato accesso

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,4 +1,6 @@
 <template>
+  loggggg:
+  {{ logged  }}
   <div class="flex px-3 flex-row justify-between items-center w-full top-0 h-25 z-50 sticky text-black text-2xl bg-white shadow-md">
     <LogoPortale v-if="isInPortale" class="pt-4"/>
     <Logo v-else class="pt-4"/>
@@ -14,6 +16,9 @@
             <RouterLink to="/register" :class="slotProps.class">Registrati</RouterLink>
           </Button>
           </div>
+          <div v-else>
+            sei loggato
+          </div>
         </div>
 
         <div class="block lg:hidden">
@@ -24,6 +29,9 @@
             <Button label="Registrati" asChild v-slot="slotProps">
             <RouterLink to="/register" :class="slotProps.class">Registrati</RouterLink>
           </Button>
+            </div>
+            <div v-else class="">
+              sei loggato 
             </div>
           </Drawer>
           <Button icon="pi pi-bars" @click="drawer = true" />
@@ -38,16 +46,27 @@
 import Logo from "./Logo.vue"
 import LogoPortale from "./LogoPortale.vue"
 import MenuNavigazione from "./MenuNavigazione.vue";
-import {  defineProps,ref, onMounted, onBeforeUnmount } from 'vue'
+import {  defineProps,ref, onMounted, onBeforeUnmount, computed} from 'vue'
 import Button from 'primevue/button';
 import Drawer from 'primevue/drawer';
 import Dialog from 'primevue/dialog';
 import LoginDialog from "../Dialogs/LoginDialog.vue";
 import { RouterLink } from "vue-router";
-
+import { useStoreUtente } from "../../stores/UserStore";
+import { useEmployeeStore } from "../../stores/EmployeeStore";
 const visible = ref(false);
 const drawer = ref(false);
-const logged= ref(false);
+const storeUtente = useStoreUtente();
+const storeEmployee = useEmployeeStore();
+
+const logged = computed(() => {
+  return false;
+  if (props.isInPortale) {
+    return storeEmployee.isAutenticato;
+  } else {
+    return storeUtente.isAutenticato;
+  }
+});
 
 const props = defineProps({
     isInPortale: Boolean
