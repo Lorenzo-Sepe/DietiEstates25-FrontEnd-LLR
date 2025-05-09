a4be3f0ba8f153ec93c81ff99694fd98711e21fa
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-05-09 17:39:02 +0200

    risolto problema che non si vedeva il form di registrazione

--- src/views/AgencyRegistrationView.vue
+++ src/views/AgencyRegistrationView.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="flex flex-col lg:flex-row justify-beween items-center lg:gap-5 mb-5 px-5">
+    <div class="flex flex-col  justify-beween items-center lg:gap-5 mb-5 px-5">
         <div>
             <h1>Benvenuto nel nostro Portale Immobiliare</h1>
             <p>
                 Benvenuto sul nostro portale, dedicato alle agenzie immobiliari e ai loro agenti. 
-                Qui puoi pubblicare i tuoi annunci e raggiungere un sacco di potenziali acquirenti. Gli utenti possono sfogliare gli annunci e inviare una proposta che, oltre all'offerta economica, include i loro contatti per essere ricontattati. In questo modo, comprare o vendere diventa davvero semplice e veloce
+                Qui puoi pubblicare i tuoi annunci e raggiungere un sacco di potenziali acquirenti. Gli utenti possono sfogliare gli annunci e inviare una proposta che, oltre all'offerta economica, include i loro contatti per essere ricontattati.
             </p>
         </div>
 
-        <Accordion value="0" class="min-w-[50vw] w-[80vw] lg:w-[50vw] mt-5">
+        <Accordion value="1" class=" w-full mt-5">
             <AccordionPanel value="1"  fluid>
                 <AccordionHeader>Registra la tua agenzia ora</AccordionHeader>
                 <AccordionContent>
