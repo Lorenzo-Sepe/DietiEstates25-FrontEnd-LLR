31b2fd6c0be01884f924b3a321082723f5b80eaa
Sperob98 <rbrna98@gmail.com>
2025-05-09 15:27:46 +0200

    Inserito le caratteristiche aggiuntive nel multiselect  sostituendoli al toggle

--- src/components/ListaAnnunci/ContenutoMenuFiltro.vue
+++ src/components/ListaAnnunci/ContenutoMenuFiltro.vue
@@ -1,6 +1,7 @@
 <template>
 
-    <div class="flex flex-col gap-8 w-full md:h-100 overflow-y-auto items-start justify-start p-2 bg-white">
+    <div
+        class="flex flex-col gap-8 w-full md:min-h-screen md:max-h-100 overflow-y-auto items-start justify-start p-2 bg-white">
 
         <div class="luogo w-full">
 
@@ -54,49 +55,9 @@
         <div class="altre-caratteristiche flex flex-col items-start justify-start gap-2">
             <label class="block text-lg font-semibold mb-1">Caratteristiche Aggiuntive</label>
 
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.balconi" />
-                <span class="">Balconi</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.garage" />
-                <span class="">Garage</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.postiAuto" />
-                <span class="">Posti auto</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.giardino" />
-                <span class="">Giardino</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.ascensore" />
-                <span class="">Ascensore</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.portiere" />
-                <span class="">Portiere</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.riscaldamentoCentralizzato" />
-                <span class="">Riscaldamento Centralizzato</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.climatizzatori" />
-                <span class="">Climatizzatori</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.pannelliSolari" />
-                <span class="">Pannelli Solari</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.cantina" />
-                <span class="">Cantina</span>
-            </div>
-            <div class="flex flex-row items-start justify-start">
-                <ToggleSwitch v-model="caratteristicheAggiuntive.soffitta" />
-                <span class="">Soffitta</span>
+            <div class="card flex justify-center">
+                <MultiSelect v-model="caratteristicheSelezionate" :options="caratteristiche" optionLabel="name" filter
+                    placeholder="Caratteristiche aggiuntive" :maxSelectedLabels="3" class="w-full md:w-80" />
             </div>
 
         </div>
@@ -114,6 +75,7 @@ import ToggleSwitch from 'primevue/toggleswitch';
 import Button from 'primevue/button';
 import SelectButton from 'primevue/selectbutton';
 import AutoComplete from 'primevue/autocomplete';
+import MultiSelect from 'primevue/multiselect';
 
 import Mappa from '../ListaAnnunci/Mappa.vue'
 
@@ -132,11 +94,11 @@ const luogoCercato = ref({ comune: route.query.comune, latitudine: route.query.l
 const selectedTipologiaImmobile = ref();
 selectedTipologiaImmobile.value = route.query.immobile ? ref({ name: route.query.immobile }) : null
 const opzioniTipologiaImmobile = ref([
-    { name: 'APPARTAMENTO'},
-    { name: 'TERRENO'},
-    { name: 'UFFICIO'},
-    { name: 'POSTOAUTO'},
-    { name: 'ALTRO'}
+    { name: 'APPARTAMENTO' },
+    { name: 'TERRENO' },
+    { name: 'UFFICIO' },
+    { name: 'POSTOAUTO' },
+    { name: 'ALTRO' }
 ]);
 const selectedTipologiaImmobileBackup = ref(route.query.immobile)
 const buckupSelectedImmobile = () => {
@@ -152,23 +114,24 @@ const valoreMetriQuadriMinimo = ref(route.query.mqMin);
 const valoreMetriQuadriMassimo = ref(route.query.mqMax);
 const valoreDistanzaMassima = ref(5);
 
-const caratteristicheAggiuntive = ref({
-
-    balconi: route.query.balconi==true || route.query.balconi=='true' ? true : false,
-    garage: route.query.garage==true || route.query.garage=='true' ? true : false,
-    postiAuto: route.query.postiAuto==true || route.query.postiAuto=='true' ? true : false,
-    giardino: route.query.giardino==true || route.query.giardino=='true' ? true : false,
-    ascensore: route.query.ascensore==true || route.query.ascensore=='true' ? true : false,
-    portiere: route.query.portiere==true || route.query.portiere=='true' ? true : false,
-    riscaldamentoCentralizzato: route.query.riscaldamentoCentralizzato==true || route.query.riscaldamentoCentralizzato=='true' ? true : false,
-    climatizzatori: route.query.climatizzatore==true || route.query.climatizzatore=='true' ? true : false,
-    pannelliSolari: route.query.pannelliSolari==true || route.query.pannelliSolari=='true' ? true : false,
-    cantina: route.query.cantina==true || route.query.cantina=='true' ? true : false,
-    soffitta: route.query.soffitta==true || route.query.soffitta=='true' ? true : false,
-})
+const caratteristicheSelezionate = ref();
+const caratteristiche = ref([
+    { name: 'Balconi' },
+    { name: 'Garage' },
+    { name: 'Posti auto' },
+    { name: 'Giardino' },
+    { name: 'Ascensore' },
+    { name: 'Portiere' },
+    { name: 'Riscaldamento centralizzato' },
+    { name: 'Climatizzatori' },
+    { name: 'Pannelli solari' },
+    { name: 'Cantina' },
+    { name: 'Soffitta' },
+]);
 
 onMounted(() => {
     CountryService.getCountries().then((data) => (countries.value = data));
+    inizializzaCaratteristicheSelezionate();
 });
 
 const search = (event) => {
@@ -184,6 +147,8 @@ const search = (event) => {
 }
 
 const onApliccaFiltro = () => {
+
+    console.log('multiselecttttt:::', caratteristicheSelezionate.value);
 
     router.push({
 
@@ -201,25 +166,68 @@ const onApliccaFiltro = () => {
             prezzoMax: valorePrezzoMassimo.value,
             mqMin: valoreMetriQuadriMinimo.value,
             mqMax: valoreMetriQuadriMassimo.value,
-            balconi: caratteristicheAggiuntive.value.balconi ? caratteristicheAggiuntive.value.balconi : null,
-            garage: caratteristicheAggiuntive.value.garage ? caratteristicheAggiuntive.value.garage : null,
-            postiAuto: caratteristicheAggiuntive.value.postiAuto ? caratteristicheAggiuntive.value.postiAuto : null,
-            giardino: caratteristicheAggiuntive.value.giardino ? caratteristicheAggiuntive.value.giardino : null,
-            ascensore: caratteristicheAggiuntive.value.ascensore ? caratteristicheAggiuntive.value.ascensore : null,
-            portiere: caratteristicheAggiuntive.value.portiere ? caratteristicheAggiuntive.value.portiere : null,
-            riscaldamentoCentralizzato: caratteristicheAggiuntive.value.riscaldamentoCentralizzato ? caratteristicheAggiuntive.value.riscaldamentoCentralizzato : null,
-            climatizzatore: caratteristicheAggiuntive.value.climatizzatori ? caratteristicheAggiuntive.value.climatizzatori : null,
-            pannelliSolari: caratteristicheAggiuntive.value.pannelliSolari ? caratteristicheAggiuntive.value.pannelliSolari : null,
-            cantina: caratteristicheAggiuntive.value.cantina ? caratteristicheAggiuntive.value.cantina : null,
-            soffitta: caratteristicheAggiuntive.value.soffitta ? caratteristicheAggiuntive.value.soffitta : null,
-            
+
+            balconi: isCaratteristicaSelezionata("Balconi") ? true : null,
+            garage: isCaratteristicaSelezionata("Garage") ? true : null,
+            postiAuto: isCaratteristicaSelezionata("Posti auto") ? true : null,
+            giardino: isCaratteristicaSelezionata("Giardino") ? true : null,
+            ascensore: isCaratteristicaSelezionata("Ascensore") ? true : null,
+            portiere: isCaratteristicaSelezionata("Portiere") ? true : null,
+            riscaldamentoCentralizzato: isCaratteristicaSelezionata("Riscaldamento centralizzato") ? true : null,
+            climatizzatore: isCaratteristicaSelezionata("Climatizzatori") ? true : null,
+            pannelliSolari: isCaratteristicaSelezionata("Pannelli solari") ? true : null,
+            cantina: isCaratteristicaSelezionata("Cantina") ? true : null,
+            soffitta: isCaratteristicaSelezionata("Soffitta") ? true : null,
+
+
         }
 
     });
 
 }
 
+const isCaratteristicaSelezionata = (caratteristica) => {
+
+    for (const elemento of caratteristicheSelezionate.value) {
+        if (elemento.name === caratteristica) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+const inizializzaCaratteristicheSelezionate = () => {
+
+    caratteristicheSelezionate.value = [];
+
+    if (route.query.balconi == true || route.query.balconi == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Balconi' });
+    if (route.query.garage == true || route.query.garage == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Garage' });
+    if (route.query.postiAuto == true || route.query.postiAuto == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Posti auto' });
+    if (route.query.giardino == true || route.query.giardino == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Giardino' });
+    if (route.query.ascensore == true || route.query.ascensore == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Ascensore' });
+    if (route.query.portiere == true || route.query.portiere == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Portiere' });
+    if (route.query.riscaldamentoCentralizzato == true || route.query.riscaldamentoCentralizzato == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Riscaldamento centralizzato' });
+    if (route.query.climatizzatore == true || route.query.climatizzatore == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Climatizzatori' });
+    if (route.query.pannelliSolari == true || route.query.pannelliSolari == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Pannelli solari' });
+    if (route.query.cantina == true || route.query.cantina == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Cantina' });
+    if (route.query.soffitta == true || route.query.soffitta == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Soffitta' });
+}
+
 watch(route, (newRoute) => {
+
+    caratteristicheSelezionate.value = [];
 
     valorePrezzoMinimo.value = route.query.prezzoMin ? parseInt(route.query.prezzoMin) : null;
     valorePrezzoMassimo.value = route.query.prezzoMax ? parseInt(route.query.prezzoMax) : null;
@@ -228,17 +236,28 @@ watch(route, (newRoute) => {
     selectedTipologiaImmobile.value = route.query.immobile ? ref({ name: route.query.immobile }) : null;
     selectedContratto.value = route.query.contratto ? route.query.contratto : null;
 
-    caratteristicheAggiuntive.value.balconi = route.query.balconi ? true : false;
-    caratteristicheAggiuntive.value.garage = route.query.garage ? true : false;
-    caratteristicheAggiuntive.value.postiAuto = route.query.postiAuto ? true : false;  
-    caratteristicheAggiuntive.value.giardino = route.query.giardino ? true : false;
-    caratteristicheAggiuntive.value.ascensore = route.query.ascensore ? true : false;
-    caratteristicheAggiuntive.value.portiere = route.query.portiere ? true : false;
-    caratteristicheAggiuntive.value.riscaldamentoCentralizzato = route.query.riscaldamentoCentralizzato ? true : false;
-    caratteristicheAggiuntive.value.climatizzatori = route.query.climatizzatore ? true : false;
-    caratteristicheAggiuntive.value.pannelliSolari = route.query.pannelliSolari ? true : false;
-    caratteristicheAggiuntive.value.cantina = route.query.cantina ? true : false;
-    caratteristicheAggiuntive.value.soffitta = route.query.soffitta ? true : false;
+    if (route.query.balconi == true || route.query.balconi == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Balconi' });
+    if (route.query.garage == true || route.query.garage == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Garage' });
+    if (route.query.postiAuto == true || route.query.postiAuto == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Posti auto' });
+    if (route.query.giardino == true || route.query.giardino == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Giardino' });
+    if (route.query.ascensore == true || route.query.ascensore == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Ascensore' });
+    if (route.query.portiere == true || route.query.portiere == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Portiere' });
+    if (route.query.riscaldamentoCentralizzato == true || route.query.riscaldamentoCentralizzato == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Riscaldamento centralizzato' });
+    if (route.query.climatizzatore == true || route.query.climatizzatore == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Climatizzatori' });
+    if (route.query.pannelliSolari == true || route.query.pannelliSolari == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Pannelli solari' });
+    if (route.query.cantina == true || route.query.cantina == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Cantina' });
+    if (route.query.soffitta == true || route.query.soffitta == 'true')
+        caratteristicheSelezionate.value.push({ name: 'Soffitta' });
 });
 
 
